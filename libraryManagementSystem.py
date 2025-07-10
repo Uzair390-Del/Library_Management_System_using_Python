12f6242de60d24a7f96f6f626cb00af8c74553a8
Uzair390-Del <uzair.zairi321@gmail.com>
2025-07-10 13:29:44 +0500

    inheritance and classes objects

--- libraryManagementSystem.py
+++ libraryManagementSystem.py
@@ -5,6 +5,7 @@ class LibraryItem:
         self.author=author
         self.isbn=isbn
         self.available=True
+
     def __str__(self):
         return f"{self.title} by {self.author}"
     
@@ -17,17 +18,35 @@ class LibraryItem:
     def check_in(self):
         print(f"Checked in : {self}")
         self.available=True
-
-
-
-
-
-
+        
 #child class of libray item
+#inheritance
 class Book(LibraryItem):
-
-
-
-
+    def __init__(self, title, author, isbn,genre):
+        super().__init__(title, author, isbn)
+        self.genre=genre
+    def __str__(self):
+        return f"{super().__str__()} (Genre: {self.genre})"
 
 class User:
+    def __init__(self,name):
+        self.name=name
+    def borrow_item(self,item):
+        item.check_out()
+    def return_item(self,item):
+        item.check_in()
+
+
+
+library = []
+book1=Book("Make up your Mind","Uzair Hassan","2001256","generation")
+library.append(book1)
+
+user1=User("Hassan")
+user2=User("Asif")
+user1.borrow_item(book1)
+user2.borrow_item(book1)
+
+print("Library item :")
+for item in library:
+   print(item)
